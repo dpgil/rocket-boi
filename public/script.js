9b96f408086c93b6700d5eb1413303380ba092e5
Daniel Gil <t-dagil@microsoft.com>
2017-04-09 23:20:45 -0400

    Working obstacles with collision detection

--- public/script.js
+++ public/script.js
@@ -90,10 +90,70 @@ var mainMenu = {
 /* -------------- END main menu -------------- */
 
 
+/* -------------- BEGIN end game -------------- */
+var gameOverMenu = {
+	playing : false,
+	message : "",
+	setup : function() {
+		console.log("Starting main menu");
+
+		//gameOverMenu.addPlayer();
+		gameOverMenu.addText();
+
+		gameOverMenu.playing = true;
+		gameOverMenu.gameLoop();
+	},
+	addText : function() {
+		gameOverMenu.message = new Text(
+			"Game over! Press space to play again",
+			{fontFamily: "Arial", fontSize: 64, fill: "white"}
+		);
+
+		// center the text
+		gameOverMenu.message.anchor.x = 0.5;
+		gameOverMenu.message.anchor.y = 0.5;
+
+		gameOverMenu.message.x = renderer.width / 2;
+		gameOverMenu.message.y = renderer.height / 2;
+
+		stage.addChild(gameOverMenu.message);
+	},
+	play : function() {
+		// next level
+		if (keyPressed[32]) {
+			gameOverMenu.nextLevel();
+		}
+	},
+	nextLevel : function() {
+		gameOverMenu.clear();
+		level1.setup();
+	},
+	clear : function() {
+		gameOverMenu.playing = false;
+		stage.removeChild(gameOverMenu.message);
+		stage.removeChild(player);
+	},
+	gameLoop : function() {
+		if (gameOverMenu.playing) {
+			// loop this function at 60 frames per second
+			requestAnimationFrame(gameOverMenu.gameLoop);
+
+			// game logic
+			gameOverMenu.play();
+
+			// render the stage to see the animation
+			renderer.render(stage);
+		}
+	}
+}
+/* -------------- END end game -------------- */
+
+
 /* -------------- BEGIN level 1 -------------- */
 var level1 = {
 	playing : false,
 	obstacles : [],
+	message : "",
 	setup : function() {
 		console.log("Starting level 1");
 
@@ -117,7 +177,10 @@ var level1 = {
 
 		var rand = Math.floor(Math.random() * (1000) + 500);
 		console.log("Spawning obstacle in " + rand + "\n");
-		setTimeout(level1.spawnObstacle, rand);
+
+		if (level1.playing) {
+			setTimeout(level1.spawnObstacle, rand);
+		}
 	},
 	addObstacle : function() {
 		// random x
@@ -133,23 +196,27 @@ var level1 = {
 			obstacle.y -= obstacle.speed;
 		}
 
-		obstacle.width = 50;
-		obstacle.height = 100;
+		obstacle.width = 100;
+		obstacle.height = 50;
 		obstacle.anchor.x = 0.5;
 		obstacle.anchor.y = 1;
 		obstacle.x = rx;
-		obstacle.y = renderer.height;
+		obstacle.y = renderer.height + obstacle.height;
 		level1.obstacles.push(obstacle);
 		stage.addChild(obstacle);
 	},
 	updateObstacles : function() {
-		level1.obstacles.forEach(function(e) {
-			if (e.y < 0) {
-				var index = level1.obstacles.indexOf(e);
+		level1.obstacles.forEach(function(obstacle) {
+			// player hit an eagle
+			if (hitTestRectangle(player, obstacle)) {
+				level1.gameOver();
+			// check if the obstacle has left the screen
+			} else if (obstacle.y < 0) {
+				var index = level1.obstacles.indexOf(obstacle);
 				level1.obstacles.splice(index,1);
-				stage.removeChild(e);
+				stage.removeChild(obstacle);
 			} else {
-				e.moveUp();
+				obstacle.moveUp();
 			}
 		});
 	},
@@ -202,6 +269,41 @@ var level1 = {
 		// adds player to the stage
 		stage.addChild(player);
 	},
+	play : function() {
+		// w
+		if (keyPressed[87]) {
+			player.moveUp();
+		}
+		// a
+		if (keyPressed[65]) {
+			player.moveLeft();
+		}
+		// s
+		if (keyPressed[83]) {
+			player.moveDown();
+		}
+		// d
+		if (keyPressed[68]) {
+			player.moveRight();
+		}
+	},
+	gameOver : function() {
+		level1.playing = false;
+
+		level1.message = new Text(
+			"Game over! Press space to play again",
+			{fontFamily: "Arial", fontSize: 64, fill: "white"}
+		);
+
+		// center the text
+		level1.message.anchor.x = 0.5;
+		level1.message.anchor.y = 0.5;
+
+		level1.message.x = renderer.width / 2;
+		level1.message.y = renderer.height / 2;
+
+		stage.addChild(level1.message);
+	},
 	nextLevel : function() {
 		level1.clear();
 		//level2.setup();
@@ -209,7 +311,14 @@ var level1 = {
 	clear : function() {
 		level1.playing = false;
 		stage.removeChild(player);
+
 		//remove everything
+		level1.obstacles.forEach(function(obstacle) {
+			stage.removeChild(obstacle);
+		});
+		level1.obstacles = [];
+		stage.removeChild(level1.message);
+		level1.message = "";
 	},
 	gameLoop : function() {
 		if (level1.playing) {
@@ -218,15 +327,80 @@ var level1 = {
 
 			level1.updateObstacles();
 			// game logic
-			//level1.play();
+			level1.play();
 
 			// render the stage to see the animation
+			renderer.render(stage);
+		} else {
+			level1.endGameLoop();
+		}
+	},
+	endGameLoop : function() {
+		if (!level1.playing) {
+			requestAnimationFrame(level1.endGameLoop);
+
+			if (keyPressed[32]) {
+				level1.clear();
+				level1.setup();
+			}
+
 			renderer.render(stage);
 		}
 	}
 
 }
 /* -------------- END level 1 -------------- */
+
+function hitTestRectangle(r1, r2) {
+
+  //Define the variables we'll need to calculate
+  var hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+
+  //hit will determine whether there's a collision
+  hit = false;
+
+  //Find the center points of each sprite
+  r1.centerX = r1.x + r1.width / 2;
+  r1.centerY = r1.y + r1.height / 2;
+  r2.centerX = r2.x + r2.width / 2;
+  r2.centerY = r2.y + r2.height / 2;
+
+  //Find the half-widths and half-heights of each sprite
+  r1.halfWidth = r1.width / 2;
+  r1.halfHeight = r1.height / 2;
+  r2.halfWidth = r2.width / 2;
+  r2.halfHeight = r2.height / 2;
+
+  //Calculate the distance vector between the sprites
+  vx = r1.centerX - r2.centerX;
+  vy = r1.centerY - r2.centerY;
+
+  //Figure out the combined half-widths and half-heights
+  combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+  combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+
+  //Check for a collision on the x axis
+  if (Math.abs(vx) < combinedHalfWidths) {
+
+    //A collision might be occuring. Check for a collision on the y axis
+    if (Math.abs(vy) < combinedHalfHeights) {
+
+      //There's definitely a collision happening
+      hit = true;
+    } else {
+
+      //There's no collision on the y axis
+      hit = false;
+    }
+  } else {
+
+    //There's no collision on the x axis
+    hit = false;
+  }
+
+  //`hit` will be either `true` or `false`
+  return hit;
+};
 
 
 // load images and starts the game
@@ -255,9 +429,9 @@ window.onkeydown = function(e) {
 	keyPressed[e.keyCode] = true;
 }
 
-onmousemove = function(e) {
-	if (level1.playing) {
-		player.x = e.clientX;
-		player.y = e.clientY;
-	}
-}
+//onmousemove = function(e) {
+//	if (level1.playing) {
+//		player.x = e.clientX;
+//		player.y = e.clientY;
+//	}
+//}
